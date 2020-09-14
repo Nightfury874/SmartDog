0e50e1e63fcf9dd68d0e997f24cb4e7a7e6a47a5
Nihar Jyoti Basisth <62636996+Nightfury874@users.noreply.github.com>
2020-09-14 18:28:16 +0530

    Update DCMDriverL298.cpp

--- DCMDriverL298.cpp
+++ DCMDriverL298.cpp
@@ -1,12 +1,7 @@
 #include <Arduino.h>
 #include "DCMDriverL298.h"
 
-/**
-* Construct a DC Motor Driver instance.<BR>
-* It constructs two DC Motor instances, motorL and motorR.<BR>
-* enA, enB - enable pins for motors. connected to PWM pin on Arduino board.<BR>
-* pinA1,pinA2,pinB1,pinB2 - direction pin of the motors. connected to digital pins on Arduino board.
-*/
+
 DCMDriverL298::DCMDriverL298(const int enA, const int pinA1, const int pinA2, const int enB, const int pinB1, const int pinB2) : m_enA(enA), m_enB(enB), m_pinA1(pinA1), m_pinA2(pinA2), m_pinB1(pinB1), m_pinB2(pinB2)
 {
 	pinMode(m_pinA1,OUTPUT);
@@ -69,3 +64,9 @@ void DCMDriverL298::off(int pinPWM, int pinDir1, int pinDir2)
 	digitalWrite(pinDir1, LOW);
 	digitalWrite(pinDir2, LOW);
 }
+
+//Code written by NIHAR JYOTI BASISTH//
+// Reusage of code is prohibited withour prior knowledge of the author//
+
+
+
